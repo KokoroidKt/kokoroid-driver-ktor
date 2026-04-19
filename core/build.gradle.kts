9be1f1bed4c2184e6d6538ffa8b9b21ac50932f9
moran0710 <3575331055@qq.com>
2026-04-19 14:56:39 +0800

    Add test cases for connections and refine connection abstractions

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -1,5 +1,12 @@
+import dev.kokoroidkt.gradle.runKokoroid.config.ExtensionTypes
+import dev.kokoroidkt.gradle.runKokoroid.config.runKokoroid
+
+
 plugins {
-    kotlin("jvm") version "2.3.10"
+    kotlin("jvm")
+    id("dev.kokoroidkt.gradle.runKokoroid") version "0.4.0"
+    id("com.gradleup.shadow") version "9.4.1"
+    alias(libs.plugins.kotlinPluginSerialization)
 }
 
 group = "dev.kokoroid"
@@ -11,9 +18,16 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
+    testImplementation(libs.mockk)
+    testImplementation(libs.kotlinxCoroutinesTest)
+    testImplementation(libs.ktor.client.mock)
     implementation(project(":api"))
     implementation(libs.bundles.kotlinxEcosystem)
     implementation(libs.bundles.kokoroidSuit)
+    implementation(libs.bundles.ktorClient)
+    implementation(libs.bundles.ktorServer)
+    implementation(platform(libs.koin.bom))
+    implementation(libs.koin.core)
 }
 
 kotlin {
@@ -22,4 +36,13 @@ kotlin {
 
 tasks.test {
     useJUnitPlatform()
+}
+
+runKokoroid {
+    extensionFilename = "core-1.0-SNAPSHOT-all.jar"
+    // i delete the github token, oops
+    isValidationOnly = false
+    skipDownload = true
+    enableKokoroidDebug = true
+    testExtensionType = ExtensionTypes.DRIVER
 }
